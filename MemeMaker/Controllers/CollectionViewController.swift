eac01e4711fee9d352a6d324730f5a91a4370373
Dmitry <dvpris06@gmail.com>
2019-12-19 17:21:41 +0300

    Add segues to DetailViewController

--- MemeMaker/Controllers/CollectionViewController.swift
+++ MemeMaker/Controllers/CollectionViewController.swift
@@ -61,36 +61,16 @@ class CollectionViewController: UICollectionViewController {
         return cell
     }
 
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "collectionViewSegueToDetail" {
+            let sendingCell = sender as! UICollectionViewCell
+            let sendingCellIndexPath = collectionView!.indexPath(for: sendingCell)!
+            let selectedMeme = sendingCellIndexPath.row
+            
+            let controller = segue.destination as! DetailViewController
+            controller.selectedMeme = MemeData.allMemes[selectedMeme]
+        }
     }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
 
 }
 
